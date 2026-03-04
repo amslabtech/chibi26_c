5fd85e1367806b2c91e73051eb7bb1c268cd5f23
Kouki-20 <shnokou06@gmail.com>
2026-03-04 15:30:59 +0900

    Delete src directory

--- src/obstacle_detector_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "obstacle_detector/obstacle_detector.hpp"
-int main(int argc, char *argv[])
-{
-    rclcpp::init(argc, argv);
-    std::shared_ptr<ObstacleDetector> od = std::make_shared<ObstacleDetector>();
-    rclcpp::spin(od);
-    rclcpp::shutdown();
-    return 0;
-}
